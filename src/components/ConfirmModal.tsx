bee549b269b5592c7611d14238ceba4def42aa7e
Alexandre PENOMBRE <alexandre.penombre@gmail.com>
2022-05-28 09:12:00 +0200

    href url

--- src/components/ConfirmModal.tsx
+++ src/components/ConfirmModal.tsx
@@ -24,7 +24,7 @@ export const ConfirmModal = ({
   return (
     <>
       <a
-        href="!#"
+        href="#!"
         className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         onClick={(e) => {
           setShow(!show);
@@ -50,7 +50,7 @@ export const ConfirmModal = ({
         >
           <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <a
-              href="!#"
+              href="#!"
               className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
               onClick={(e) => setShow(false)}
             >
@@ -62,7 +62,7 @@ export const ConfirmModal = ({
                 {message}
               </h3>
               <a
-                href="!#"
+                href="#!"
                 data-modal-toggle="popup-modal"
                 className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
                 onClick={() => {
@@ -83,7 +83,7 @@ export const ConfirmModal = ({
                 {confirmLabel ?? "Confirm"}
               </a>
               <a
-                href="!#"
+                href="#!"
                 data-modal-toggle="popup-modal"
                 className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
                 onClick={() => {
